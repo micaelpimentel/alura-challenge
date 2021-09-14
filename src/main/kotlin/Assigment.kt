a0689f482dcac591dc7bdb14a5df491f9c17000b
Micael Pimentel <micael.pimentel@eldorado.org.br>
2021-09-14 00:40:14 -0400

    Added Assigment

---
+++ src/main/kotlin/Assigment.kt
@@ -0,0 +1,17 @@
+class Assigment(
+    val name: String,
+    val code: String,
+    val ordinal: Int,
+    val isActive: Boolean = false,
+    val type: Int,
+    val section: Section
+) {
+    init {
+        if (name.isBlank())
+            throw IllegalArgumentException("Nome do curso não pode ser vazio")
+        if (code.isBlank())
+            throw IllegalArgumentException("Código não pode ser vazio")
+        if (!code.all { it.isLowerCase() && it.isLetter() || it == '-' })
+            throw IllegalArgumentException("Código deve ser composto somente por letras minusculas e -")
+    }
+}
